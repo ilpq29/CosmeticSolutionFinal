b4416e959384382fab1f206587b4eb0ba1377667
chalokim <chalokim@naver.com>
2021-01-11 07:16:09 +0900

    MapControl에 지하철역 표시 기능 및 범위 표시

--- CosmeticSolutionFinal/Recommendation500mAreaMap.cs
+++ CosmeticSolutionFinal/Recommendation500mAreaMap.cs
@@ -9,18 +9,26 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using CosmeticSolutionFinal.Data;
 using CosmeticSolutionFinal.Data.Models;
+using DevExpress.XtraMap;
+using DevExpress.XtraEditors;
 
 namespace CosmeticSolutionFinal
 {
     public partial class Recommendation500mAreaMap : Form
     {
+        private string bingKey = "AnarxWHrNxOAE7_aT1ydFrZ1eVvFqxq_9-ZyigJYu-aRDeOzuKGysCRsbiBRKkAU";
         public Recommendation500mAreaMap()
         {
             InitializeComponent();
+            this.bingMapDataProvider1.BingKey = this.bingKey;
+            this.bingGeocodeDataProvider.BingKey = this.bingKey;
+            this.buttonSearch.Click += buttonSearch_Click;
         }
-        private void Recommendation500mAreaMap_Load(object sender, EventArgs e)
+        #region 버튼 이벤트
+        private void comboSearchAddress_Click(object sender, EventArgs e)
         {
-
+            ListAddressInfo();
+            return;
         }
         private void comboSearchAddress_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -28,8 +36,30 @@ namespace CosmeticSolutionFinal
             {
                 string stationName = Convert.ToString(comboSearchAddress.SelectedValue.ToString());
                 ListStationInfo(stationName);
+              
             }
+            return;
+        }    
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            if (comboSearchStation.SelectedItem != null)
+            {
+                string stationName = Convert.ToString(comboSearchStation.SelectedItem.ToString());
+                ListCoordinatesInfo(stationName);
+                BingMap.Zoom(15);
+                BingMap.ZoomToFitLayerItems(BingMap.Layers.Where(l => l is VectorItemsLayer));               
+            }
+            return;      
         }
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            BingMap.Layers.Where(l => l is VectorItemsLayer);
+            BingMap.CenterPoint = new GeoPoint(37.559418939678949, 126.991928283803932);
+            BingMap.ZoomLevel = 10;
+        }
+        #endregion
+
+        #region 지역 / 지하철 역 / 좌표 class
         public void ListAddressInfo()
         {
             comboSearchAddress.DataSource = StationDao.GetAddress();
@@ -45,59 +75,53 @@ namespace CosmeticSolutionFinal
         }
         public void ListCoordinatesInfo(string station)
         {
-            List<StationModel> coordinatesList = StationDao.GetInfo(station);
-
-/*            comboSearchStation.Items.Clear();
-*/            foreach (var x in coordinatesList)
-            {
-                MessageBox.Show(x.Latitude.ToString());
-                MessageBox.Show(x.Longitude.ToString());
-            }
-        }
-            
-        private void comboSearchAddress_Click(object sender, EventArgs e)
-        {
-            ListAddressInfo();
-        }
-
-        private void comboSearchStation_SelectedIndexChanged(object sender, EventArgs e)
-        {          
-                 
-        }
-
-        /*private List<StationModel> LoadData(string address)
-        {
-            List<StationModel> coordinatesList = StationDao.GetInfo(address);          
-            if (comboSearchStation.SelectedValue != null)
-            {
-                address = Convert.ToString(comboSearchStation.SelectedValue);
-
-            }
-
-            return XDocument.Load(path).Element("Ships").Elements("Ship")
-                .Select(e => new ShipwreckData(
-                    year: Convert.ToInt32(e.Element("Year").Value, CultureInfo.InvariantCulture),
-                    name: e.Element("Name").Value,
-                    description: e.Element("Description").Value,
-                    latitude: Convert.ToDouble(e.Element("Latitude").Value, CultureInfo.InvariantCulture),
-                    longitude: Convert.ToDouble(e.Element("Longitude").Value, CultureInfo.InvariantCulture)
-                ))
-                .ToList();
-        }*/
-        private void buttonSearch_Click(object sender, EventArgs e)
-        {
-            if (comboSearchStation.SelectedItem != null)
-            {
-                string stationName = Convert.ToString(comboSearchStation.SelectedItem.ToString());
-                ListCoordinatesInfo(stationName);
-            }
-        }
-
-        private void comboBox1_Click(object sender, EventArgs e)
-        {
-        
+            List<StationModel> coordinatesList = StationDao.GetInfo(station);           
+            double? latitude, longitude;
+            foreach (var x in coordinatesList)
+            {            
+                latitude = x.Latitude;
+                longitude = x.Longitude;
                 
-            
+                this.bingGeocodeDataProvider.RequestLocationInformation(new GeoPoint((double)latitude, (double)longitude), null);
+
+                MapBubble((double)latitude, (double)longitude);
+            }
         }
+        #endregion
+
+        #region MapControl 지도에 범위 표시
+        public void MapBubble(double latitude, double longitude)
+        {
+            MapItemStorage itemStorage = new MapItemStorage();
+
+            itemStorage.Items.Add
+                (
+                    new MapBubble()
+                    {
+                        Argument = "A",
+                        Value = 100,
+                        Location = new GeoPoint(latitude, longitude),
+                        Size = 100,
+                        Group = 1,
+                        MarkerType = MarkerType.Circle
+                    }
+                );
+
+            KeyColorColorizer keyColorColorizer = new KeyColorColorizer();
+            keyColorColorizer.ItemKeyProvider = new ArgumentItemKeyProvider();
+            keyColorColorizer.Colors.Add(Color.FromArgb(75, Color.Red));
+            keyColorColorizer.Keys.Add(new ColorizerKeyItem() { Key = "A", Name = "Category A" });
+
+            VectorItemsLayer vectorItemsLayer = new VectorItemsLayer();
+            vectorItemsLayer.Data = itemStorage;
+            vectorItemsLayer.Colorizer = keyColorColorizer;
+            this.BingMap.Layers.Add(vectorItemsLayer);
+
+/*            ColorListLegend colorListLegend = new ColorListLegend();
+            colorListLegend.Layer = vectorItemsLayer;
+            this.mapControl.Legends.Add(colorListLegend);*/
+        }
+        #endregion
+      
     }
 }
